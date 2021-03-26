21be86bec4ed83c6744c3ac5ea607d405efe573d
amrgharz <ammor.ghr@gmail.com>
2021-03-27 01:33:48 +0400

    apply shuffle functionality

--- js/index.js
+++ js/index.js
@@ -5,4 +5,13 @@ $(function () {
     upperH = $(".upper-nav").innerHeight(),
     navH = $(".navbar").innerHeight();
   $(".slider, .carousel-item").height(winH - (upperH + navH));
+  $(".featured-work ul li").on("click", function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    if ($(this).data("class") === "all") {
+      $(".shuffel-images .col-sm").css("opacity", 1);
+    } else {
+      $(".shuffel-images .col-sm").css("opacity", ".09");
+      $($(this).data("class")).css("opacity", 1);
+    }
+  });
 });
